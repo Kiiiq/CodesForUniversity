f4d0b1a0a46b1f0ca728ba79264ead342d307c3a
Kiq <kiq.ramosds@gmail.com>
2025-06-04 22:10:32 -0300

    Trabalho 2 POO
    Trabalho 2 (Basicamente) Finalizado

---
+++ Trabalho 2 POO/RoupaTamanhoUnico.java
@@ -0,0 +1,17 @@
+public class RoupaTamanhoUnico extends Peca implements Item {
+    
+    public RoupaTamanhoUnico(String descricao, int estoqueMax, int estoqueMin){
+        super(descricao, estoqueMax, estoqueMin);
+    }
+
+
+    @Override
+    public void venda() {
+        
+        qntdEstoque--;
+        
+        if(qntdEstoque<estoqueMin){
+            reestoque();
+        }
+    }
+}
